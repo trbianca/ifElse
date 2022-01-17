e396d8a8ad67f460076c485fb05d8025836904ce
trbianca <trbianca@outlook.com>
2022-01-17 16:59:47 -0300

    Desafio 02 If Else - MOD2 - Bianca Trindade

---
+++ script.js
@@ -0,0 +1,69 @@
+//1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+
+//let idade = 60;
+
+//if(idade >= 18){
+  //  console.log("Você é maior de idade, sinto muito!")
+//} else{
+  //  console.log("Você não é maior de idade, curta enquanto há tempo!")
+//}
+
+//OU TAMBÉM PODE SER FEITO
+//let idade = prompt("Informe sua idade");
+
+//if(idade >= 18){
+  //  console.log("Você é maior de idade, sinto muito");
+//} else {
+  //  console.log("Você não é maior de idade, curta enquanto há tempo!")
+//}
+
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+
+//let idade = 18;
+//let cond = true;
+
+//if((idade >= 18) & (cond= true)){
+  //  console.log("Você é um humano maior de idade")
+//} else{
+   // console.log("Permetido apenas para humanos maiores de idade")
+//}
+
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+
+//let jan = true;
+//let dez = true;
+
+//if((jan= true) || (dez= false)){
+  //  console.log("Você é pode ser de capricórnio")
+//} else{
+  //  console.log("Não é nem Janeiro e nem dezembro")
+//}
+
+
+
+//*** Exercícios bônus
+//4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+//let inicialNome = true;
+
+//if(inicialNome= true){
+ // console.log("Seu nome começa com R");
+
+//} else{
+  //  console.log("Seu nome não começa com R");
+//}
+
+
+//OU TAMBÉM PODE SER FEITO 
+//let inicialNome = prompt("Qual a inicial do seu nome? ");
+
+//if(inicialNome == "R"){
+  //  console.log("Seu nome começa com a letra R")
+
+//} else{
+   // console.log("Seu nome não começa com a letra R")
+//}
+
+//5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
+
+
